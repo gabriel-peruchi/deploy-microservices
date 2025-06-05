ed38d52037ec49a5c7071ee4f72520a12684c7ff
gabriel-peruchi <gabriel-peruchi@hotmail.com>
2025-06-04 22:15:08 -0300

    refactor: initial structure app orders

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,7 +1,36 @@
-import Fastify from 'fastify'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
-const server = Fastify()
+const server = fastify().withTypeProvider<ZodTypeProvider>()
 
-server.listen({ port: 3000 }, () => {
-  console.log('app-orders is running on http://localhost:3000')
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.get('/health', () => {
+  return 'OK'
+})
+
+server.post(
+  '/orders',
+  {
+    schema: {
+      body: z.object({
+        amount: z.number(),
+      }),
+    },
+  },
+  (request, reply) => {
+    const { amount } = request.body
+    console.log(`Received order with amount: ${amount}`)
+    return reply.status(200).send()
+  },
+)
+
+server.listen({ host: '0.0.0.0', port: 3000 }, () => {
+  console.log('[Orders] HTTP Server running!')
 })
