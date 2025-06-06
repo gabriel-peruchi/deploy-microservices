fc04de3c7e917658392448ced6040de7eb8e97cd
gabriel-peruchi <gabriel-peruchi@hotmail.com>
2025-06-05 21:02:27 -0300

    refactor: remove files

--- app-orders copy/src/broker/messages/order-created.ts
+++
@@ -1,6 +0,0 @@
-import { OrderCreatedMessage } from '../../../../contracts/messages/order-created-message.ts'
-import { channels } from '../channels/index.ts'
-
-export function dispacthOrderCreated(data: OrderCreatedMessage) {
-  channels.orders.sendToQueue('orders', Buffer.from(JSON.stringify(data)))
-}
